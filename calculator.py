0f3135a9ef43f750c046c6c31c86421e6a4a8f15
nueadkie <chris.j.miraflor@gmail.com>
2021-04-30 22:14:02 -0700

    added error-checking to avoid divide by zero

--- calculator.py
+++ calculator.py
@@ -2,7 +2,9 @@ def calc(a, b):
   calc_sum = a + b
   diff = a - b
   multiple = a * b
-  quotient = a / b
+  quotient = None
+  if b != 0:
+    quotient = a / b
   nums = {
     "sum": calc_sum,
     "diff": diff,
