73d10e9379cf0d77a0d5e6a98687ad65b715f6bf
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-05-02 18:20:28 -0700

    fixed tests not using calculator function definition

--- test_calculator.py
+++ test_calculator.py
@@ -3,23 +3,23 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
   def test_sum(self):
-    result = calc(1,2)["sum"]
+    result = calculator.calc(1,2)["sum"]
     self.assertEqual(result, 3)
 
   def test_diff(self):
-    result = calc(5, 3)["diff"]
+    result = calculator.calc(5, 3)["diff"]
     self.assertEqual(result, 2)
 
   def test_multiple(self):
-    result = calc(3, 8)["multiple"]
+    result = calculator.calc(3, 8)["multiple"]
     self.assertEqual(result, 24)
 
   def test_quotient(self):
-    result = calc(7, 2)["quotient"]
+    result = calculator.calc(7, 2)["quotient"]
     self.assertEqual(result, 3.5)
 
   def test_div_by_zero(self):
-    result = calc(2, 0)["quotient"]
+    result = calculator.calc(2, 0)["quotient"]
     self.assertEqual(result, None)
 
 
